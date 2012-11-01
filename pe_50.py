3384b2bfec94494ff7a92d4cda186c93cee7baee
Philip Hayes <philiphayes9@gmail.com>
2012-11-01 10:50:34 -0700

    added solution to problem 50

---
+++ pe_50.py
@@ -0,0 +1,19 @@
+# find the greatest prime under 1000000 that is the sum of consecutive primes
+
+from pe_utils import prime_sieve
+from itertools import takewhile
+
+max_sum = 10000000
+primes = list(prime_sieve(max_sum))
+num_primes = len(primes)
+
+sums = list(takewhile(lambda x: x < max_sum, (sum(primes[:x]) for x in range(num_primes))))
+num_sums = len(sums)
+
+max_len = 0
+def sum_and_bump(i, j):
+    global max_len
+    max_len = j - i
+    return sums[j] - sums[i]
+
+print(max([sum_and_bump(i, j) for i in range(num_sums) for j in range(i + 1 + max_len, num_sums) if j - i > max_len]))
