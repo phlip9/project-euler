9236066c9115442d7341dc9a511547078ed71443
Philip Hayes <philiphayes9@gmail.com>
2012-11-04 17:02:15 -0800

    did 19 again for some reason

--- pe_19.py
+++ pe_19.py
@@ -1,16 +1,10 @@
 from datetime import timedelta, date
 
-cur = date(1901, 1, 1)
-end = date(2000, 12, 31)
+def date_gen(cur, lim):
+    while cur < lim:
+        cur += timedelta(days=1)
+        yield cur
 
-num_sundays = 0
-
-while cur <= end:
-	if cur.day == 1 and cur.weekday() == 6:
-		print(cur)
-		num_sundays += 1
-	cur += timedelta(days = 1)
-
-print("Sundays on 1st of month from 1900-1-1 to 2000-12-31:", num_sundays)
+print(len([d for d in date_gen(date(1901, 1, 1), date(2000, 12, 31)) if d.day == 1 and d.weekday() == 6]))
 
 # 171
